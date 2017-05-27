5f70aea39c9f36bfe695cdb2dfd82cfe1572e65e
rww_2010 <rww_2010@126.com>
2017-05-27 09:40:01 +0800

    aa

--- app/src/main/java/com/zgreenmatting/utils/GetBigFileMD5.java
+++ app/src/main/java/com/zgreenmatting/utils/GetBigFileMD5.java
@@ -5,18 +5,8 @@ import org.apache.commons.codec.binary.Hex;
 import java.io.File;
 import java.io.FileInputStream;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class GetBigFileMD5 {
-
-    public static MessageDigest MD5 = null;
-
-    static {
-        try {
-            MD5 = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException ne) {
-        }
-    }
 
     /**
      * 对一个文件获取md5值
@@ -26,13 +16,14 @@ public class GetBigFileMD5 {
     public static String getMD5(File file) {
         FileInputStream fileInputStream = null;
         try {
+            MessageDigest MD5 = MessageDigest.getInstance("MD5");
             fileInputStream = new FileInputStream(file);
             byte[] buffer = new byte[8192];
             int length;
             while ((length = fileInputStream.read(buffer)) != -1) {
                 MD5.update(buffer, 0, length);
             }
-            return new String(Hex.encodeHex(MD5.digest()));
+            return String.valueOf(Hex.encodeHex(MD5.digest()));
         } catch (Exception e) {
             e.printStackTrace();
             return null;
